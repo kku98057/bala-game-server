70e8bdb663704a568a9f2654d6e87d66206df60a
김기태 <kku98057@naver.com>
2024-11-01 23:14:03 +0900

    fix.s3.config수

--- src/config/s3.config.ts
+++ src/config/s3.config.ts
@@ -2,6 +2,13 @@ import { S3Client } from "@aws-sdk/client-s3";
 import multer from "multer";
 import multerS3 from "multer-s3";
 import { Request } from "express";
+
+const AWS_S3_BUCKET = process.env.AWS_S3_BUCKET;
+if (!AWS_S3_BUCKET) {
+  console.error("AWS_S3_BUCKET is missing");
+  process.exit(1); // 또는 기본값 설정
+}
+
 export const s3 = new S3Client({
   region: process.env.AWS_REGION,
   credentials: {
@@ -9,22 +16,22 @@ export const s3 = new S3Client({
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
   },
 });
+
+// S3 스토리지 설정을 함수로 분리
+const s3Storage = multerS3({
+  s3: s3,
+  bucket: AWS_S3_BUCKET,
+  key: function (req: Request, file: Express.Multer.File, cb) {
+    const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+    cb(null, `balance-game/${uniqueSuffix}-${file.originalname}`);
+  },
+  contentType: multerS3.AUTO_CONTENT_TYPE,
+});
+
 export const upload = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: process.env.AWS_S3_BUCKET!,
-    key: function (
-      req: Request,
-      file: Express.Multer.File,
-      cb: (error: Error | null, key?: string) => void
-    ) {
-      const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
-      cb(null, `balance-game/${uniqueSuffix}-${file.originalname}`);
-    },
-    contentType: multerS3.AUTO_CONTENT_TYPE,
-  }),
+  storage: s3Storage,
   limits: {
-    fileSize: 10 * 1024 * 1024, // 10MB
+    fileSize: 10 * 1024 * 1024,
     files: 32,
   },
 });
