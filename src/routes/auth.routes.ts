3de25b3463662995dec0b40c7e1e6d006ce946e4
김기태 <kku98057@naver.com>
2024-11-08 10:43:03 +0900

    feat.유저정보 API추가

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,5 +1,11 @@
 import express from "express";
-import { login, logout, register } from "../controllers/auth.controller";
+import {
+  getUser,
+  login,
+  logout,
+  register,
+} from "../controllers/auth.controller";
+import { authMiddleware } from "../middleware/auth.middleware";
 
 const router = express.Router();
 // 회원가입 라우트
@@ -8,4 +14,7 @@ router.post("/register", register);
 // 로그인 라우트
 router.post("/login", login);
 router.post("/logout", logout);
+
+// 유저정보 조회
+router.get("/user", authMiddleware, getUser);
 export default router;
